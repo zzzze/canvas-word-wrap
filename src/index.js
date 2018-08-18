8147dd6cf4c654a2107a6f889677ab7cbd406b18
zhangzeshuan <zhangzeshuan@ifanr.com>
2018-08-18 16:47:17 +0800

    add unit test

--- src/index.js
+++ src/index.js
@@ -1,93 +1,110 @@
-const canvas = window.document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-
-function splitWord(text) {
-  const array = text.split('')
-  const reg = /[\d\w]/
-  return array.reduce((newArray, item) => {
-    if (!reg.test(item) || reg.test(item) && !reg.test(newArray[newArray.length - 1])) {
-      newArray.push(item)
-    } else if (!newArray.length) {
-      newArray.push(item)
-    } else {
-      newArray[newArray.length - 1] += item
-    }
-    return newArray
-  }, [])
+function splitWord(text, wordBreak) {
+  text = text.trim()
+  if (wordBreak === 'break-all') {
+    return text.split('')
+  } else {
+    var array = text.split('')
+    var reg = /[\d\w]/
+    return array.reduce(function(newArray, item) {
+      if (!reg.test(item) || reg.test(item) && !reg.test(newArray[newArray.length - 1])) {
+        newArray.push(item)
+      } else if (!newArray.length) {
+        newArray.push(item)
+      } else {
+        newArray[newArray.length - 1] += item
+      }
+      return newArray
+    }, [])
+  }
 }
-const string = 'fdsfewg官方代购 范德萨fads here'
-console.log(splitWord(string))
 
-ctx.rect(0, 0, 100, 100)
-ctx.fill()
-function getTextLine(ctx, text, fontSize, containerWidth, {
-  textOverflow = 'ellipsis',
-} = {}) {
-  let left = text
-  let right = ''
-  let result = ''
-  let rest = ''
-  ctx.font = `${fontSize}px Arial`
+function getTextLine(ctx, text, fontSize, containerWidth, options) {
+  text = text.trim()
+  options = options || {}
+  var textOverflow = options.textOverflow
+  var wordBreak = options.wordBreak || 'keep-all'
+  var left = splitWord(text, wordBreak)
+  var right = []
+  var result = []
+  var rest = []
+  ctx.font = '' + fontSize + 'Arial'
 
-  const splitText = text => {
-    const count = Math.ceil(text.length / 2)
-    left = text.slice(0, count)
-    right = text.slice(count)
+  var splitArray = function(textArray) {
+    var count = Math.ceil(textArray.length / 2)
+    left = textArray.slice(0, count)
+    right = textArray.slice(count)
+  }
+
+  var joinText = function(textArrayA, textArrayB) {
+    return textArrayA.concat(textArrayB).join('')
   }
 
   while (true) {
-    const width = ctx.measureText(result + left).width
+    var width = ctx.measureText(joinText(result, left)).width
     if (width <= containerWidth) {
-      result =  result + left
-      splitText(right)
+      result =  result.concat(left) 
+      splitArray(right)
+    } else if (left.length === 1) {
+      if (!result.length) {
+        result = left
+        rest = right.concat(rest)
+      } else {
+        rest = left.concat(right, rest)
+      }
+      break
     } else {
-      rest = right + rest
-      splitText(left)
+      rest = right.concat(rest)
+      splitArray(left)
     }
-    if (left.length <= 1) break
   }
 
-  if (textOverflow === 'ellipsis') {
-    const postFix = '...'
-    const postFixLength = postFix.length 
-    result = result.slice(0, result.length - 1) + postFix
+  if (typeof textOverflow === 'string' && textOverflow !== '' || typeof textOverflow === 'undefined') {
+    if (textOverflow === 'ellipsis' || typeof textOverflow === 'undefined') {
+      var postFix =['...']
+    } else {
+      var postFix = textOverflow
+    }
+    result = result.join('').split('')
+    result = result.slice(0, result.length - 1).concat(postFix)
     while (true) {
-      const width = ctx.measureText(result).width
+      var width = ctx.measureText(result).width
       if (width <= containerWidth) {
         break
       } else {
-        result = result.slice(0, result.length - 1 - postFixLength) + postFix
+        result = result.slice(0, result.length - 2).concat(postFix)
       }
     }
-  }
+  } else if (textOverflow !== '') {
 
-  return {result, rest}
+  }
+  return {result: result.join('').trim(), rest: rest.join('').trim()}
 }
 
-function wrap(ctx, text, fontSize, containerWidth, {
-  lineClamp = -1,
-  textOverflow = 'ellipsis',
-} = {}) {
-  ctx.font = `${fontSize}px`
-  const resultArray = []
-  let rest = text
-  let i = 0
+function wrap(ctx, text, fontSize, containerWidth, options) {
+  options = options || {}
+  var lineClamp = options.lineClamp || -1
+  var textOverflow = options.textOverflow
+  var wordBreak = options.wordBreak || 'keep-all'
+  ctx.font = '' + fontSize + 'px'
+  var resultArray = []
+  var rest = text
+  var i = 0
   while (true) {
-    const _textOverflow = i >= lineClamp - 1 && lineClamp > 0 ? textOverflow : '' 
     if (ctx.measureText(rest).width <= containerWidth) {
       resultArray.push(rest)
       break
     } else {
-      const {result: _result, rest: _rest} = getTextLine(ctx, rest, fontSize, containerWidth, {textOverflow: _textOverflow})
-      resultArray.push(_result)
-      rest = _rest
+      var textLineObj = getTextLine(ctx, rest, fontSize, containerWidth, {
+        textOverflow: i >= lineClamp - 1 && lineClamp > 0 ? textOverflow : '', wordBreak})
+      var result = textLineObj.result
+      var rest = textLineObj.rest
+      resultArray.push(result)
     }
     i += 1
     if (i == lineClamp) break
   }
   return resultArray
 }
- 
-const text = '方法范德萨范德萨热热挖人而为额为他人'
-const textArray = wrap(ctx, text, 20, 100, {lineClamp: 2})
-console.log(textArray)
+
+module.exports = wrap
+
---
+++ test/index.test.js
@@ -0,0 +1,46 @@
+var chai = require('chai')
+var wrap = require('../src/index.js')
+var expect = chai.expect
+
+describe('word wrap', function () {
+  var canvas = document.createElement('canvas')
+  var ctx = canvas.getContext('2d')
+  var text = 'abc 123 一二三四五六七 abc 123 一二三四五六七 abc 123 一二三四五六七'
+
+  it('should return correct count of lines', function () {
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 1}).length).to.equal(1)
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 2}).length).to.equal(2)
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 3}).length).to.equal(3)
+  })
+
+  it('should handle text overflow correctly', function () {
+    var re = /\.{3}$/
+    expect(function () {
+      var result = wrap(ctx, text, 20, 100, {lineClamp: 1})
+      return re.test(result[0])
+    }()).to.be.ok
+
+    expect(function () {
+      var result = wrap(ctx, text, 20, 100, {lineClamp: 1, textOverflow: ''})
+      return re.test(result[0])
+    }()).to.not.be.ok
+
+    expect(function () {
+      var result = wrap(ctx, text, 20, 100, {lineClamp: 1, textOverflow: 'xyz'})
+      return /xyz$/.test(result[0])
+    }()).to.be.ok
+  })
+
+  it('should return correct result', function () {
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 1})).to.eql(['abc 123 一...'])
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 2})).to.eql(['abc 123 一二三四五', '六七 abc 12...'])
+  })
+
+
+
+  it('should handle word break correctly', function () {
+    var text = 'abc 123 def abc 1234 ijk abc 123 xyz'
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 2})).to.eql(['abc 123 def abc', '1234 ijk ab...'])
+    expect(wrap(ctx, text, 20, 100, {lineClamp: 2, wordBreak: 'break-all'})).to.eql(['abc 123 def abc 123', '4 ijk abc 123 xyz'])
+  })
+})
